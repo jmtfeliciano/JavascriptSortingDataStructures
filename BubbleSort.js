226eb971682d586b0e19cd9d96b3e1341599937f
neonseri <jfeliciano@fas.harvard.edu>
2017-06-15 23:59:09 -0400

    fixed spacing

--- BubbleSort.js
+++ BubbleSort.js
@@ -5,16 +5,16 @@ var numberTraversal = a.length - 1;
 
 while( numberTraversal > 0 ){
 	
-  for(var i = 0; i < numberTraversal; i++){
+	for(var i = 0; i < numberTraversal; i++){
   
-  	if( a[i] > a[i+1] ){
-    	var tempValue = a[i];
-      a[i]  = a[i+1];
-      a[i+1] = tempValue;
-      console.log(a); 
-      //console will print every time a change is made
-    }
-  }
+  		if( a[i] > a[i+1] ){
+    			var tempValue = a[i];
+      			a[i]  = a[i+1];
+     			a[i+1] = tempValue;
+      			console.log(a); 
+      			//console will print every time a change is made
+    		}
+ 	 }
   
-  numberTraversal--;
+ 	 numberTraversal--;
 }
