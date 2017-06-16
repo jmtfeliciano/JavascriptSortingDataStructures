345a21ec012960d5b997fb09ef042d7e5d5f8986
neonseri <jfeliciano@fas.harvard.edu>
2017-06-16 18:41:46 -0400

    Included pseudocode in my innermost loop

--- InsertionSort.js
+++ InsertionSort.js
@@ -1,2 +1,27 @@
 //Basic JS code to implement and demonstrate insertion sort
 
+var arrayy = [1,9,2,3];
+
+var noIterations = arrayy.length - 1;
+
+
+while(noIterations>0){
+	
+  for(var i=1;i<noIterations;i++){
+  
+  	var lowestIndex = i;
+    
+    for(var k=i;k>0;k--){
+    	if(arrayy[k]<arrayy[k-1]){
+      	
+        
+        
+      }
+    }
+    
+   
+    
+  }
+  
+  noInterations--;
+}
