efa511c72440f904495f184672a27eca4a97953a
neonseri <jfeliciano@fas.harvard.edu>
2017-06-20 14:31:19 -0400

    Uncommented the bottom part of my code for implementation

--- InsertionSort.js
+++ InsertionSort.js
@@ -29,7 +29,7 @@ while(noIterations>0){
 }
 
 
-/*
+
 if (arrayy.length == 1){
 	console.log(arrayy);
 }else if(arrayy.length == 2){
@@ -41,4 +41,4 @@ if (arrayy.length == 1){
   }else{
   	console.log(arrayy);
   }
-} */
+} 
