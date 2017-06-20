2429d8e1350db807284278af0ce710b14cbdb15d
neonseri <jfeliciano@fas.harvard.edu>
2017-06-20 14:23:07 -0400

    Implemented and Wrote Full Code
    There is a bug:  if arrayy[0] > arrayy[1], the calculation gets super messed up!

--- InsertionSort.js
+++ InsertionSort.js
@@ -2,27 +2,44 @@
 //NOTE:  Arrayy[k] should not be modified as it compares value.  
 // Need to identify which index to be inserted THEN MODIFY
 var arrayy = [1,9,2,3];
+console.log("Original array is: "+arrayy); // will log original array
 
 var noIterations = arrayy.length - 1;
-
+var currentIndex = 1;
 
 while(noIterations>0){
 	
-  for(var i=1;i<noIterations;i++){
-  
-  	var lowestIndex = i;
-    
-    for(var k=i;k>0;k--){
-    	if(arrayy[k]<arrayy[k-1]){
-      	
-        
-        
-      }
-    }
-    
+	var whereToInsert = currentIndex;	
    
-    
-  }
-  
-  noIterations--;
+	for(var i=currentIndex; i > 0; i--){
+      
+		if(arrayy[i-1] <= arrayy[currentIndex]){
+      
+      		if(whereToInsert !== currentIndex && i === 1 ){
+        		arrayy.splice(whereToInsert,0,arrayy[currentIndex]);
+        	  arrayy.splice(currentIndex+1,1);
+         	 console.log(arrayy);
+        	}
+        
+      	}else{
+      		whereToInsert = i-1;
+      	}
+	}	
+	currentIndex++;
+	noIterations--;
 }
+
+
+/*
+if (arrayy.length == 1){
+	console.log(arrayy);
+}else if(arrayy.length == 2){
+	if(arrayy[0] > arrayy[1]){
+  	var temp = arrayy[0];
+    arrayy[0] = arrayy[1];
+    arrayy[1] = temp;
+    console.log(arrayy);
+  }else{
+  	console.log(arrayy);
+  }
+} */
