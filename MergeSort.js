2cbe47df021d9ef92b9f75c240ed8db445aba5a7
neonseri <josemari.feliciano@gmail.com>
2017-06-28 02:55:25 -0400

    added more comments/notes for approach

--- MergeSort.js
+++ MergeSort.js
@@ -1,3 +1,15 @@
 //File to run and implement merge sort
 //probably want to use some form of recursion since i want to get individual parts.
 //the difficult part:  how to string them back together again (need to think about this)
+
+function mergeSort(array){
+
+  if(array.length===1){
+    return array[0];
+  }else{
+    var value = mergeSort(array.slice(0,array.length / 2)+mergeSort(array.slice(array.length / 2));
+    //still unsure how to work the code above                      
+  }
+  
+  
+}
