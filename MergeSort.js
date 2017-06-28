21b1d34c6ec78afc397584531584e208ba7a56cd
neonseri <jfeliciano@fas.harvard.edu>
2017-06-27 22:10:11 -0400

    commented strategy for implementation

--- MergeSort.js
+++ MergeSort.js
@@ -1,1 +1,3 @@
 //File to run and implement merge sort
+//probably want to use some form of recursion since i want to get individual parts.
+//the difficult part:  how to string them back together again (need to think about this)
